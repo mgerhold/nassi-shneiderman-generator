b09a302d05a3514600e6b349b56b9e42d094eaae
Michael Gerhold <michael.gerhold@web.de>
2025-07-22 17:40:04 +0200

    feat: implement branches

--- main.py
+++ main.py
@@ -1,24 +1,71 @@
+from symbols import MonadicSelective, DyadicSelective
 from typing import Final
 
 from diagram import Diagram
 from latex import render_latex_and_show
-from symbols import Imperative, Serial, Block
+from symbols import Imperative, Serial, Block, MultipleExclusiveSelective, Branch
 
 
 def main():
     diagram: Final = Diagram(
-        Block(
-            text="Mein Block",
-            inner=Serial(
-                elements=[
-                    Imperative(r"Eingabe: \texttt{n}"),
-                    Imperative(r"$\texttt{n} := \texttt{n} + 1$"),
-                    Imperative(r"Was geht ab? $\frac{1}{2}$"),
-                    Imperative(r"a"),
-                    Imperative(r"-"),
-                ]
-            )
-        )
+        MultipleExclusiveSelective(
+            common_condition_part="Weiß der Geier?",
+            branches=[
+                Branch(
+                    condition="Ja",
+                    inner=MonadicSelective(
+                        common_condition_part="War geil?",
+                        then=Branch(
+                            condition="Ja",
+                            inner=Serial(
+                                elements=[
+                                    Imperative("Dann!"),
+                                    Imperative("Auch."),
+                                    Imperative("Und!"),
+                                ]
+                            ),
+                        ),
+                    ),
+                ),
+                Branch(
+                    condition="Vielleicht",
+                    inner=DyadicSelective(
+                        common_condition_part="Wie geht's?",
+                        then=Branch(
+                            condition="Gut",
+                            inner=Imperative("Okay."),
+                        ),
+                        else_=Branch(
+                            condition="Schlecht",
+                            inner=Imperative("Ciao."),
+                        ),
+                    )
+                ),
+                Branch(
+                    condition="Nein",
+                    inner=Imperative("Nö :("),
+                ),
+                Branch(
+                    condition="Bla",
+                    inner=Imperative("Dann eben doch."),
+                ),
+                Branch(
+                    condition="Nein",
+                    inner=Block(
+                        text="Mein Block",
+                        inner=Serial(
+                            elements=[
+                                Imperative(r"Eingabe: \texttt{n}"),
+                                Imperative(r"$\texttt{n} := \texttt{n} + 1$"),
+                                Imperative(r"Was geht ab? $\frac{1}{2}$"),
+                                Imperative(r"a"),
+                                Imperative(r"-"),
+                            ]
+                        ),
+                    ),
+                ),
+            ],
+        ),
     )
     latex_code: Final = diagram.emit()
     print(latex_code)
--- symbols/__init__.py
+++ symbols/__init__.py
@@ -1,3 +1,4 @@
+from typing import NamedTuple
 from typing import override
 from dimensions import Dimensions
 from latex import measure_latex_dimensions
@@ -6,6 +7,27 @@ from abc import ABC, abstractmethod
 from typing import Optional
 
 _TEXT_MARGIN = 4.0
+
+def _lerp(start: float, end: float, t: float) -> float:
+    return start + (end - start) * t
+
+def _options(color: Optional[str]) -> str:
+    return "" if color is None else f"[{color}]"
+
+
+def _line(x1: float, y1: float, x2: float, y2: float) -> str:
+    return rf"\draw ({x1}pt, {y1}pt) -- ({x2}pt, {y2}pt);" + "\n"
+
+
+def _rectangle(x1: float, y1: float, x2: float, y2: float, color: Optional[str]) -> str:
+    return rf"\draw{_options(color)} ({x1}pt,{y1}pt) rectangle ({x2}pt, {y2}pt);" + "\n"
+
+
+def _text(position: tuple[float, float], text: str, color: Optional[str] = None) -> str:
+    size: Final = measure_latex_dimensions(text)
+    x: Final = position[0] + size.width / 2.0
+    y: Final = position[1] - size.height / 2.0 - size.depth / 2.0
+    return rf"\node{_options(color)} at ({x}pt, {y}pt) {{{text}}};" + "\n"
 
 
 class Symbol(ABC):
@@ -35,12 +57,12 @@ class Imperative(Symbol):
         y1: Final = position[1]
         x2: Final = position[0] + size[0]
         y2: Final = position[1] - size[1]
-        text_x: Final = position[0] + size[0] / 2.0
-        text_y: Final = position[1] - size[1] / 2.0 - self._dimensions.depth / 2.0
-        options: Final = "" if self._color is None else f"[{self._color}]"
-        return rf"""\draw{options} ({x1}pt,{y1}pt) rectangle ({x2}pt, {y2}pt);\
-\node at ({text_x}pt, {text_y}pt) {{{self.text}}};
-"""
+        text_size: Final = measure_latex_dimensions(self._text)
+        text_x: Final = position[0] + _TEXT_MARGIN
+        text_y: Final = position[1] - size[1] / 2.0 + text_size.height / 2.0
+        return _rectangle(x1, y1, x2, y2, self._color) + _text(
+            (text_x, text_y), self._text, self._color
+        )
 
     @override
     @property
@@ -74,18 +96,20 @@ class Block(Symbol):
         text_size: Final = measure_latex_dimensions(self._text)
         return text_size.total_height + _TEXT_MARGIN * 2.0
 
+    @override
     def emit(self, position: tuple[float, float], size: tuple[float, float]) -> str:
-        result = Imperative("").emit(position, size)
-        text_size: Final = measure_latex_dimensions(self._text)
-        text_x: Final = position[0] + text_size.width / 2.0 + _TEXT_MARGIN
-        text_y: Final = position[1] - text_size.height / 2.0 - text_size.depth / 2.0 - _TEXT_MARGIN
-        result += rf"\node at ({text_x}pt, {text_y}pt) {{{self.text}}};" + "\n"
-        result += self._inner.emit(
-            (position[0] + self._margin, position[1] - self._margin),
-            (size[0] - 2.0 * self._margin, size[1] - 2.0 * self._margin),
+        return (
+            Imperative("").emit(position, size)
+            + _text(
+                (position[0] + _TEXT_MARGIN, position[1] - _TEXT_MARGIN), self._text
+            )
+            + self._inner.emit(
+                (position[0] + self._margin, position[1] - self._margin),
+                (size[0] - 2.0 * self._margin, size[1] - 2.0 * self._margin),
+            )
         )
-        return result
 
+    @override
     @property
     def required_size(self) -> tuple[float, float]:
         inner_size: Final = self._inner.required_size
@@ -104,10 +128,10 @@ class Serial(Symbol):
     def elements(self) -> list[Symbol]:
         return self._elements
 
+    @override
     def emit(self, position: tuple[float, float], size: tuple[float, float]) -> str:
         output = ""
-        required_size: Final = self.required_size
-        element_size: Final = required_size[0], required_size[1] / len(self._elements)
+        element_size: Final = size[0], size[1] / len(self._elements)
         current_position = position
         for element in self._elements:
             output += element.emit(current_position, element_size)
@@ -127,3 +151,139 @@ class Serial(Symbol):
             max_width = max(max_width, element_size[0])
             max_height = max(max_height, element_size[1])
         return max_width, max_height * len(self._elements)
+
+
+@final
+class Branch(NamedTuple):
+    condition: str
+    inner: Symbol
+
+
+class MultipleExclusiveSelective(Symbol):
+    def __init__(self, common_condition_part: str, branches: list[Branch]):
+        self._common_condition_part = common_condition_part
+        self._branches = branches
+
+    @property
+    def common_condition_part(self) -> str:
+        return self._common_condition_part
+
+    @property
+    def branches(self) -> list[Branch]:
+        return self._branches
+
+    @override
+    def emit(self, position: tuple[float, float], size: tuple[float, float]) -> str:
+        result = Imperative("").emit(position, (size[0], self._header_height))
+        branch_width: Final = size[0] / len(self.branches)
+        branch_height: Final = size[1] - self._header_height
+
+        # Diagonal line from top left to bottom "almost" right.
+        x1: Final = position[0]
+        y1: Final = position[1]
+        x2: Final = position[0] + (len(self.branches) - 1) * branch_width
+        y2: Final = position[1] - self._header_height
+
+        def linear_function(x: float) -> float:
+            m: Final = (y2 - y1) / (x2 - x1)
+            b: Final = y1 - m * x1
+            return m * x + b
+
+        x3: Final = position[0] + size[0]
+        y3: Final = position[1]
+        result += _line(x1, y1, x2, y2)
+        result += _line(x2, y2, x3, y3)
+
+        for i in range(len(self.branches) - 2):
+            x = position[0] + (i + 1) * branch_width
+            result += _line(x, linear_function(x), x, position[1] - self._header_height)
+
+        for i in range(len(self.branches) - 1):
+            text = self.branches[i].condition
+            text_dimensions = measure_latex_dimensions(text)
+            x_min = position[0] + i * branch_width + _TEXT_MARGIN
+            if len(self.branches) == 2:
+                x = x_min
+            else:
+                x_centered = position[0] + (i + 0.5) * branch_width - text_dimensions.width / 2.0
+                t = i / max(len(self.branches) - 2, 1)
+                x = _lerp(x_centered, x_min, t)
+            result += _text(
+                (
+                    x,
+                    position[1] - self._header_height + _TEXT_MARGIN + text_dimensions.height,
+                ),
+                text,
+            )
+
+        text = self.branches[-1].condition
+        text_dimensions = measure_latex_dimensions(text)
+        result += _text(
+            (
+                position[0] + size[0] - text_dimensions.width - _TEXT_MARGIN,
+                position[1]
+                - self._header_height
+                + _TEXT_MARGIN
+                + text_dimensions.height,
+            ),
+            text,
+        )
+
+        m_x: Final = (position[0] + size[0] / 2.0 + position[0] + size[0] / 2.0 + x2) / 3.0
+        m_y: Final = (position[1] + position[1] + y2) / 3.0
+        text = self.common_condition_part
+        text_dimensions = measure_latex_dimensions(text)
+        result += _text(
+            (
+                m_x - text_dimensions.width / 2.0,
+                m_y + text_dimensions.height / 2.0 + text_dimensions.depth / 2.0,
+            ),
+            text,
+        )
+
+        position = (position[0], position[1] - self._header_height)
+        for branch in self.branches:
+            result += branch.inner.emit(position, (branch_width, branch_height))
+            position = (position[0] + branch_width, position[1])
+        return result
+
+    @override
+    @property
+    def required_size(self) -> tuple[float, float]:
+        return self._total_width, self._total_height
+
+    @property
+    def _header_height(self) -> float:
+        max_line_height: Final = max(
+            measure_latex_dimensions(self.common_condition_part).total_height,
+            max(
+                measure_latex_dimensions(condition.condition).total_height
+                for condition in self.branches
+            ),
+        )
+        return len(self.branches) * (max_line_height + 2.0 * _TEXT_MARGIN)
+
+    @property
+    def _body_height(self) -> float:
+        return max(condition.inner.required_size[1] for condition in self.branches)
+
+    @property
+    def _total_height(self) -> float:
+        return self._header_height + self._body_height
+
+    @property
+    def _total_width(self) -> float:
+        return len(self.branches) * max(
+            condition.inner.required_size[0] for condition in self.branches
+        )
+
+
+class DyadicSelective(MultipleExclusiveSelective):
+    def __init__(self, common_condition_part: str, then: Branch, else_: Branch) -> None:
+        super().__init__(common_condition_part, [then, else_])
+
+
+@final
+class MonadicSelective(DyadicSelective):
+    def __init__(self, common_condition_part: str, then: Branch) -> None:
+        super().__init__(common_condition_part, then, Branch("", Imperative("")))
