f9e27c92aa43f31f91d7b04505c52791fee79358
Michael Gerhold <michael.gerhold@web.de>
2025-07-26 13:33:27 +0200

    feat: make minimum diagram width customizable

--- src/nassi_shneiderman_generator/diagram.py
+++ src/nassi_shneiderman_generator/diagram.py
@@ -1,17 +1,20 @@
-from typing import final
+from typing import final, Optional, Final
 
 from nassi_shneiderman_generator.symbols import Symbol
 
 
 @final
 class Diagram:
-    def __init__(self, contents: Symbol) -> None:
+    def __init__(self, contents: Symbol, min_width: float = 80.0) -> None:
         self._contents = contents
+        self._min_width = min_width
 
     def emit(self) -> str:
         result = "\\begin{tikzpicture}\n"
+        required_size: Final = self._contents.required_size
         result += self._contents.emit(
-            position=(0, 0), size=self._contents.required_size
+            position=(0, 0),
+            size=(max(self._min_width, required_size[0]), required_size[1]),
         )
         result += "\\end{tikzpicture}\n"
         return result
